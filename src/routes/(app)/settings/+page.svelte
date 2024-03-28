67257feeced1b0f04933988d63c4631c43ce6200
Isaac-Goff <158597448+Isaac-Goff@users.noreply.github.com>
2024-03-28 13:42:38 -0500

    Week 9 - Settings
    Worked on starting settings page

---
+++ src/routes/(app)/settings/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+    function changeAvatar() {
+        console.log("This has to be made still")
+    }
+
+    function changePassword() {
+        console.log("This also has to be made")
+    }
+
+    function changeAccount() {
+        console.log("Guess What, Not Here Yet")
+    }
+</script>
+
+<div class="body">
+    <div class="main">
+        <h1>Settings</h1>
+        <div style="height: 45px; max-width: 389px; min-width: 341px">
+        <button on:click = '{changeAccount}'>
+            Change Account
+        </button>
+        </div>
+
+        <div style="height: 45px; max-width: 389px; min-width: 341px">
+        <button on:click='{changeAvatar}'>
+            Change Avatar
+        </button>
+        </div>
+
+        <div style="height: 45px; max-width: 389px; min-width: 341px">
+        <button on:click= '{changePassword}'>
+            Change Password
+        </button>
+        </div>
+    </div>
+</div>
+
+<style>
+   :global(:root) {
+      --gray: #17253e;
+      --white: #f5f7fa;
+      --green: #02cd8c;
+    }
+    .body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100vw;
+        flex-direction: column;
+    }
+    .main {
+        width: 99%;
+        height: 100%;
+        background-color: var(--gray);
+        color: var(--white);
+        border-radius: 22.83px;
+        background-clip: padding-box;
+        border: 2px solid;
+        padding: 3.5%;
+    }
+    h1 {
+        letter-spacing: 0.6px;
+        font-family: 'Iosevka', sans-serif;
+        font-size: 40px;
+    }
+</style>
