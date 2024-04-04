aa92709856e6800c8a1d7c5301ebf09b8f14607e
Isaac-Goff <158597448+Isaac-Goff@users.noreply.github.com>
2024-04-04 10:58:23 -0500

    More Button Formatting
    I should have done this all at once rather than in several commits.  Didn't know how long I'd be working on this.

--- src/routes/(app)/settings/avatar/+page.svelte
+++ src/routes/(app)/settings/avatar/+page.svelte
@@ -1,41 +1,71 @@
-<div class="body">
-    <div class="main">
-        <h1>Avatar</h1>
-
-        <button onclick="">Change Avatar</button>
-    </div>
+<div class="body-page">
+    <section class='card'>
+        <div class="prose">
+            <h1>Avatar</h1>
+        </div>
+        <form
+            class="flex flex-col gap-4 my-6"
+            method="POST"
+            action="?/login"
+        >
+            <div>
+                <button onclick="">Change Avatar</button>
+            </div>
+    </section>
 </div>
 
 <style>
-   :global(:root) {
+    :global(:root) {
       --gray: #17253e;
       --white: #f5f7fa;
       --green: #02cd8c;
     }
-    .body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        width: 100vw;
-        flex-direction: column;
+    .body-page {
+        position: relative;
+		display: flex;
+		width: 100%;
+        height: 90vh;
+		justify-content: center;
+		align-items: center;
     }
-    .main {
-        width: 99%;
-        height: 100%;
+    .card {
         background-color: var(--gray);
         color: var(--white);
-        border-radius: 22.83px;
-        background-clip: padding-box;
+        border-radius: 11.83px;
         border: 2px solid;
-        padding: 3.5%;
+        width: 35%;
+		position: relative;
+		padding: 5%;
+        padding-top: 2.5rem;
+        padding-bottom: 0;
+        min-width: 300px;
     }
-    h1 {
+    .card form input {
+        background-color: var(--gray);
+        color: var(--white);
+        border-radius: 11.83px;
+		padding: 0.5rem;
+		background-clip: padding-box;
+        border: 2px solid;
         letter-spacing: 0.6px;
-        font-family: 'Iosevka', sans-serif;
-        font-size: 40px;
+		font-family: 'Iosevka', sans-serif;
+        font-size: 1.0rem;
+        width: 100%;
+	}
+
+    .card form .input-wrapper {
+        position: relative;
     }
-    button {
+    .card form .visiblity { 		
+        height: 32px;
+        width: 32px;
+        box-shadow: none;
+        position: absolute;
+        top: 6px;
+        right: 6px;
+        opacity: 0.33;
+    }
+    .card form button {
 		cursor: pointer;
 		display: flex;
 		flex-direction: row;
@@ -47,7 +77,7 @@
 		line-height: 1;
 		color: var(--white);
 		font-size: 18px;
-		width: 50%;
+		width: 100%;
 		height: 55px;
 		font-weight: bold;
 		border: 2px solid;
@@ -56,9 +86,41 @@
 		background-color: var(--gray);
 		border-radius: 11.83px;
 	}
-    button:hover {
+    .card form button:hover {
 		box-shadow: 0 0 black;
 		color: var(--gray);
 		background-color: var(--white);
+	}
+    .card h1 {
+        letter-spacing: 0.6px;
+        font-family: 'Iosevka', sans-serif;
+        font-size: 2rem;
+		margin: 0;
+		text-align: center;
+    }
+    .card form {
+		display: flex;
+		flex-direction: column;
+		font-family: 'Iosevka', sans-serif;
+	}
+    .card form label {
+		font-family: 'Iosevka', sans-serif;
+		font-size: 1.2rem;
+		padding-bottom: 1rem;
+	}
+    .card a {
+        color: var(--green);
+        letter-spacing: 0.6px;
+        font-family: 'Iosevka', sans-serif;
+        font-size: 1.2rem;
+    }
+    @media (max-width: 1250px) {
+		.card {
+			width: 60%;
+		}
+
+		.card form input {
+			font-size: 1.25rem;
+		}
 	}
 </style>
